52b6787086cb7eaa2d82b8dbb4967b52349bf1dc
shine-rain-snow <sven916@hotmail.com>
2019-09-12 13:42:28 +0900

    left sidebar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-// import { MainComponent } from './main/main.component';
-// import { IntroComponent } from './intro/intro.component';
-// import { AstrologyComponent } from './astrology/astrology.component';
-// import { TarotComponent } from './tarot/tarot.component';
-// import { AboutComponent } from './about/about.component';
-// import { ContactComponent } from './contact/contact.component';
-// import { CounselingComponent } from './counseling/counseling.component';
+import { MaterialModule } from './material/material.module';
+
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { SidenavListComponent } from './shared/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // MainComponent,
-    // IntroComponent,
-    // AstrologyComponent,
-    // TarotComponent,
-    // AboutComponent,
-    // ContactComponent,
-    // CounselingComponent
+    NavigationComponent,
+    SidenavListComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,   
     FormsModule,
     AppRoutingModule,
+    MaterialModule,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/navigation/navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  @Output() public sidenavToggle = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onToggleSidenav = () => {
+    this.sidenavToggle.emit();
+  }
+}
