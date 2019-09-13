9cb57852f62031d2d45f1fe556d3be83a4534f96
shine-rain-snow <sven916@hotmail.com>
2019-09-13 20:20:14 +0900

    vertical photo carousel complete

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -7,15 +7,117 @@ declare var $: any;
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
-  	$(document).ready(() => {
-      $('.photo_1').css({
-        'background-color': 'yellow', 
-        'font-size': '200%'
-      });
-    });
+     setInterval(this.movePhoto, 4000);
+  }
+  movePhoto() {
+    if($("span:first-child").hasClass("active")){
+     
+        $("span").css({
+          visibility:'visible'
+        });
+        $("span:nth-child(3)").css({
+          visibility: 'hidden'
+        });
+        $(".photo").animate({
+            top: '-=100%'
+          },
+          {
+            duration: 3000,
+            complete: function() {
+              
+              $("span:nth-child(3)").css({
+                top: '100%'
+              });
+              $("span:nth-child(2)").css({
+                top: '0%'
+              });
+              $("span:nth-child(1)").css({
+                top: '100%'
+              });
+              $(".photo").css({
+                top: '0%'
+              });
+              $("span:first-child").removeClass('active');
+              $("span:nth-child(2)").addClass('active');
+              $("span:nth-child(2)").removeClass('prev');
+              $("span:nth-child(3)").addClass('prev');
+            }
+          });
+          
+    }
+
+    if($("span:nth-child(2)").hasClass("active")){
+     
+         $("span").css({
+          visibility:'visible'
+          });
+          $("span:nth-child(1)").css({
+            visibility: 'hidden'
+          });
+          $(".photo").animate({
+            top: '-=100%'
+          },
+          {
+            duration: 3000,
+            complete: function() {
+              
+              $("span:nth-child(1)").css({
+                top: '100%'
+              });
+              $("span:nth-child(3)").css({
+                top: '0%'
+              });
+              $("span:nth-child(2)").css({
+                top: '100%'  
+              });
+              $(".photo").css({
+                top: '0%'
+              });
+              $("span:nth-child(2)").removeClass('active');
+              $("span:nth-child(3)").addClass('active');
+              $("span:nth-child(3)").removeClass('prev');
+              $("span:nth-child(1)").addClass('prev');
+            }
+          });
+          
+    }
+
+    if($("span:nth-child(3)").hasClass("active")){
+      
+         $("span").css({
+          visibility:'visible'
+          });
+          $("span:nth-child(2)").css({
+            visibility: 'hidden'
+          });
+        $(".photo").animate({
+            top: '-=100%'
+          },
+          {
+            duration: 3000,
+            complete: function() {
+              
+              $("span:nth-child(1)").css({
+                top: '0%'
+              });
+              $("span:nth-child(3)").css({
+                top: '100%'
+              });
+              $("span:nth-child(2)").css({
+                top: '100%'  
+              });
+              $(".photo").css({
+                top: '0%'
+              });
+              $("span:nth-child(3)").removeClass('active').prev().prev().addClass('active');
+              $("span:first-child").removeClass('prev');
+              $("span:nth-child(2)").addClass('prev');
+            }
+          });
+    }
   }
 }
