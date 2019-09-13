5675f9ab06bc08a02d67cc9581d7ddafe61219c7
shine-rain-snow <sven916@hotmail.com>
2019-09-13 12:37:06 +0900

    jquery works, if bootstrap, there is transition error

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+declare var $: any;
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
@@ -10,6 +11,11 @@ export class IntroComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	$(document).ready(() => {
+      $('.photo_1').css({
+        'background-color': 'yellow', 
+        'font-size': '200%'
+      });
+    });
   }
-
 }
